d52e346b49223e853797325ddadd92e33df5836c
harundarat <harundarat@gmail.com>
2024-06-16 13:50:02 +0700

    Add 'coba' route to index.js

---
+++ backend/routes/cobaRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post("/", upload.single('image'));
+
+module.exports = router;
