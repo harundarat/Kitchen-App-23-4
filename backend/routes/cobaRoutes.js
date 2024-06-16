1f6b133440684cf05c80c14dcf0a9daf2883edcb
harundarat <harundarat@gmail.com>
2024-06-16 14:01:49 +0700

    e

--- backend/routes/cobaRoutes.js
+++ backend/routes/cobaRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+const { cobaUpload } = require("../controllers/cobaController");
 
-router.post("/", upload.single('image'));
+router.post("/", upload.single('image'), cobaUpload);
 
 module.exports = router;
