018a86de7c1b870e04f305989b913f187d6fd9eb
Harun Al Rasyid <harundarat@gmail.com>
2026-08-02 07:12:16 +0700

    chore: configure flowbite react integration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeInit } from "../.flowbite-react/init";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -9,6 +10,7 @@ if (!root) throw new Error("Root element not found");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
+    <ThemeInit />
     <Router>
       <App />
     </Router>
